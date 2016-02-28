89c27a4448ee5c73e0274850538a85d00737728a
MoeSadoon <msadoon590@gmail.com>
2016-02-28 19:40:22 +0000

    refactored test so cell always instantiated before each test

--- spec/conway_spec.rb
+++ spec/conway_spec.rb
@@ -1,14 +1,13 @@
 require 'spec_helper'
 
 describe 'conway' do
+  let(:cell) {Cell.new }
 
   it 'cell can be made alive' do
-    cell = Cell.new
     expect(cell.alive).to be true
   end
 
   it 'cell can be killed' do
-    cell = Cell.new
     cell.kill
     expect(cell.alive).to be false
   end
